55df736569412c2528b77bcccf1996ec00aa84b7
Toby Ho <airportyh@gmail.com>
2014-04-08 15:16:31 -0400

    Added support for camelCase.

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 module.exports = function setStyles(elm, styles){
   var lines = []
   for (var prop in styles){
-    lines.push(prop + ': ' + String(styles[prop]) + ';')
+    lines.push(deCamelCase(prop) + ': ' + String(styles[prop]) + ';')
   }
   elm.style.cssText = lines.join('')
+}
+
+function deCamelCase(str) {
+  // stolen from <https://github.com/LeaVerou/prefixfree/blob/gh-pages/prefixfree.js#L151>
+  return str.replace(/[A-Z]/g, function($0) { return '-' + $0.toLowerCase() });
 }
